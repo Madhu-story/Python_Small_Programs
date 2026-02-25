4c8ec9ac11e0199ee5107dc04c372d8382842283
Madhu-story <madhura25.rm@gmail.com>
2026-02-25 11:19:26 +0530

    Added Program 5, different swapping methods

--- Prog_5_SwapNumbers.py
+++ Prog_5_SwapNumbers.py
@@ -6,10 +6,38 @@ print("-------Before swapping--------")
 print("a: ", a)
 print("b: ", b)
 
-# swapping
+# swapping : Method 1
 c = a
 a = b
 b = c
-print("-------After swapping--------")
+print("-------After swapping- Method 1--------")
 print("a: ", a)
 print("b: ", b)
+
+# swapping : Method 2 (Using mathematical operators)
+a1 = 5
+b1 = 6
+a1 = a1 + b1
+b1 = a1 - b1
+a1 = a1 - b1
+print("-------After swapping- Method 2--------")
+print("a1: ", a1)
+print("b1: ", b1)
+
+# swapping : Method 3 (Using mathematical operators)
+a2 = 5
+b2 = 6
+a2 = a2 ^ b2
+b2 = a2 ^ b2
+a2 = a2 ^ b2
+print("-------After swapping- Method 3--------")
+print("a2: ", a2)
+print("b2: ", b2)
+
+# swapping : Method 4 (Tuple packing and unpacking)
+a3 = 5
+b3 = 6
+a3, b3 = b3, a3
+print("-------After swapping- Method 3--------")
+print("a3: ", a3)
+print("b3: ", b3)
