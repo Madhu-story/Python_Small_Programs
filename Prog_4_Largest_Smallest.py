3859d8483aac1479fccd33a46860a8a3a0c9218d
Madhu-story <madhura25.rm@gmail.com>
2026-02-25 11:07:59 +0530

    Added - Program 5, Swapping of variables

---
+++ Prog_4_Largest_Smallest.py
@@ -0,0 +1,3 @@
+# Find the largest and smallest number in the given list. (Witout using max() and min())
+
+ 
---
+++ Prog_5_SwapNumbers.py
@@ -0,0 +1,15 @@
+# Swapping of Variables:
+
+a = 5
+b = 6
+print("-------Before swapping--------")
+print("a: ", a)
+print("b: ", b)
+
+# swapping
+c = a
+a = b
+b = c
+print("-------After swapping--------")
+print("a: ", a)
+print("b: ", b)
